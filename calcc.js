c67d9978299fb4c6a08d4652d4ae71d48b5ce295
Shreshth26-glitch <shreshth.dev.he@gmail.com>
2026-03-12 06:12:29 +0530

    changed designs

--- calcc.js
+++ calcc.js
@@ -1,27 +1,53 @@
-let input = document.getElementById("inputbox");
-let buttons = document.querySelectorAll("button");
+const buttons = document.querySelectorAll("button");
 
-let string = "";
-let arr = Array.from(buttons);
-arr.forEach(button => {
-    button.addEventListener("click", (e) => {
-        if(e.target.innerHTML == "=") {
-            string = eval(string);
-            input.value = string;
-        }
+buttons.forEach(button => {
 
-        else if(e.target.innerHTML == "AC") {
-            string = "";
-            input.value = string;
-        }
-        else if(e.target.innerHTML == "DEL") {
-            string = string.substring(0, string.length-1);
-            input.value = string;
-        }
-        else {
-        string += e.target.innerHTML;
-        input.value = string;
-        }
+button.addEventListener("click", function(e){
 
-    })
+/* RIPPLE EFFECT */
+
+const circle = document.createElement("span");
+circle.classList.add("ripple");
+
+const diameter = Math.max(button.clientWidth, button.clientHeight);
+const radius = diameter / 2;
+
+circle.style.width = circle.style.height = `${diameter}px`;
+
+circle.style.left = `${e.clientX - button.offsetLeft - radius}px`;
+circle.style.top = `${e.clientY - button.offsetTop - radius}px`;
+
+button.appendChild(circle);
+
+setTimeout(()=>{
+circle.remove();
+},600);
+
+
+/* PARTICLE EXPLOSION */
+
+for(let i=0;i<6;i++){
+
+const particle = document.createElement("span");
+particle.classList.add("particle");
+
+const x = (Math.random() - 0.5) * 100;
+const y = (Math.random() - 0.5) * 100;
+
+particle.style.setProperty("--x",`${x}px`);
+particle.style.setProperty("--y",`${y}px`);
+
+particle.style.left = `${e.offsetX}px`;
+particle.style.top = `${e.offsetY}px`;
+
+button.appendChild(particle);
+
+setTimeout(()=>{
+particle.remove();
+},600);
+
+}
+
+});
+
 });
